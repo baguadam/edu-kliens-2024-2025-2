f7acc8c38e2782dbf2757c6eccc8ce03101af879
baguadam <m3wdo5@inf.elte.hu>
2025-02-11 10:37:04 +0100

    Correct typos, format with prettier

--- GY1/scripts/alternatives.js
+++ GY1/scripts/alternatives.js
@@ -1,11 +1,11 @@
 //*****************************************/
-// 2. FELADAT / afghgfhgfhgfhdfgdfgfdgfdg
+// 2. FELADAT / a
 //*****************************************/
-const navElem = document.querySelector("nav"); 
+const navElem = document.querySelector("nav");
 
 const onScroll = () => {
-    const isApplied = window.scrollY > 200;
-    navElem.classList.toggle("navbar-scrolled", isApplied);
+  const isApplied = window.scrollY > 200;
+  navElem.classList.toggle("navbar-scrolled", isApplied);
 };
 
 // ez volt a legelső, legprimitívebb megoldás a 2. feladatra, ami tulajdonképpen csak annyiban tér el
@@ -20,30 +20,30 @@ window.addEventListener("scroll", onScroll);
 // ami gondolati szinten közelít már ahhoz, amit el szeretnénk érni: ne hajtsuk végre olyan sokszor a callbacket.
 let waiting = false;
 const onScrollWithWaiting = () => {
-    if (!waiting) {
-        const isApplied = window.scrollY > 200;
-        navElem.classList.toggle("navbar-scrolled", isApplied);
-        
-        setTimeout(() => waiting = false, 1000); // 1mp után lefut, visszaállítja a waitinget false-re
-        waiting = true; // waiting true lesz, így megakadályozva a további végrehajtást az elkövetkezendő másodpercben
-    }
+  if (!waiting) {
+    const isApplied = window.scrollY > 200;
+    navElem.classList.toggle("navbar-scrolled", isApplied);
+
+    setTimeout(() => (waiting = false), 1000); // 1mp után lefut, visszaállítja a waitinget false-re
+    waiting = true; // waiting true lesz, így megakadályozva a további végrehajtást az elkövetkezendő másodpercben
+  }
 };
 
 //*****************************************/
 // 3. feladat
 //*****************************************/
-// kezdetben megnéztük lebutatíva a dolgot, bevettünk csupán egy darab elemet, amit figyelni szeretnénk, ez volt a 
+// kezdetben megnéztük lebutatíva a dolgot, bevettünk csupán egy darab elemet, amit figyelni szeretnénk, ez volt a
 // services id-jú section h2-je. Mindent ugyanúgy csináltunk, mint a végső megoldásban, csak itt beégetett animációt adtunk meg
 const serviceHeader = document.querySelector("#services h2");
 
 const onObserve = (entries) => {
-    entries.forEach(entry => {
-        // ...
-    })
+  entries.forEach((entry) => {
+    // ...
+  });
 };
 
 const observer = new IntersectionObserver(onObserve, {
-    threshold: 0.0,
+  threshold: 0.0,
 });
 
 observer.observe(serviceHeader);
